ae85b9ef3aac85720d1a4214966e0915dc1a1261
SithumKaveesha1 <sithumkaveesha1212@gmail.com>
2026-03-09 11:31:25 +0530

    3 page adding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Button } from "./components/ui/button";
+import { UNSAFE_createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter(
+  {
+    path : "/",
+    element : <App />
+  }
+
+
+
+);
 
 const App = () => {
   return (
     <div className="text-red-700 text-5xl font-bold">
       hello world
+      <Button variant="outline" size="lg">
+        Click me
+      </Button>
     </div>
   );
 };
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Signup = () => {
+return (
+<div>
+    Signup page
+</div>
+
+)
+}
+
+export default Signup
