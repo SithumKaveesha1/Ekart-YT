df072ad01180378b82989f6afb202968ed230354
SithumKaveesha1 <sithumkaveesha1212@gmail.com>
2026-03-09 10:25:02 +0530

    changes

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -51,3 +51,14 @@ export const isAuthenticated = async (req, res, next) => {
         });
     }
 };
+
+export const isAdmin = async (req, res, next) => {
+    if(req.user && req.user.role === "admin"){
+        next();
+    } else {
+        res.status(403).json({
+            success: false,
+            message: "Access denied : admin only"
+        })
+    }
+}
