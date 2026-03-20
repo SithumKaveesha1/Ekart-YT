5c1f119621d1444502742bc440e62de13a89cb82
SithumKaveesha1 <sithumkaveesha1212@gmail.com>
2026-03-20 09:28:21 +0530

    add logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,34 @@
+import { ShoppingCart } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const user = true
+
     return (
-        <div>
-            Navbar
-        </div>
+        <header className='bg-pink-50 fixed w-full z-20 border-b border-pink-200'>
+            <div className='max-w-7xl mx-auto flex justify-between items-center py-3'>
+                {/* logo section */}
+                <div>
+                    <img src="/Ekart.png" alt="" className='w-[100px]' />
+                </div>
+
+                {/* nav section */}
+                <nav className='flex gap-10 justify-between items-center'>
+                    <ul className='flex gap-7 items-center text-xl font-semibold'>
+                        <Link to={'/'}><li>Home</li></Link>
+                        <Link to={'/products'}><li>Products</li></Link>
+                        {
+                            user && <Link to={'/profile'}><li>Hello User</li></Link>
+                        }
+                    </ul>
+                    <Link to={'/cart'} className='relative'>
+                        <ShoppingCart />
+                        <span className='bg-pink-500 rounded-full absolute text-white -top-3 -right-5 px-2'>0</span>
+                    </Link>
+                </nav>
+            </div>
+        </header>
     )
 }
 
